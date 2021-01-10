34ae50e6c209e244d196edb4db979b06cf090a6a
yowgerald <solongerald.personal.flexi@gmail.com>
2021-01-10 23:14:55 +0800

    using smtp server

--- config/settings.py
+++ config/settings.py
@@ -138,20 +138,19 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 # for offline mailing
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = 'sent_emails'
+# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# EMAIL_FILE_PATH = 'sent_emails'
 
 
 # for existing smtp server
-# TODO: need change EMAIL_HOST_USER
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# MAILER_EMAIL_BACKEND = EMAIL_BACKEND
-# EMAIL_HOST = 'smtpout.secureserver.net'
-# EMAIL_HOST_PASSWORD = 'Togobi720'
-# EMAIL_HOST_USER = 'geraldsolon@togobi.com'
-# EMAIL_PORT = 465
-# EMAIL_USE_SSL = True
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+MAILER_EMAIL_BACKEND = EMAIL_BACKEND
+EMAIL_HOST = 'smtpout.secureserver.net'
+EMAIL_HOST_PASSWORD = 'Togobi720'
+EMAIL_HOST_USER = 'geraldsolon@togobi.com'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
