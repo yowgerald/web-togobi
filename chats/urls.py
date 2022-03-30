c29178ce991ab33b8454710bf0602b9ad91b3b91
yowgerald <solongerald.personal@gmail.com>
2022-03-30 17:16:19 +0800

    add react project for chats

--- chats/urls.py
+++ chats/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('message', views.message_add, name='message_add'),
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def message_add(request):
+    return render(request, 'chats/messages.html', {})
