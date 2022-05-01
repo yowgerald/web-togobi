5bc4fc8b8d6b19f15286f73ca81ac7732a36bc49
yowgerald <solongerald.personal.flexi@gmail.com>
2022-05-01 13:13:20 +0000

    partial prep for message list

--- chats/urls.py
+++ chats/urls.py
@@ -4,7 +4,7 @@ from . import views
 from . import apis
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='messages'),
     path('room/<int:target_id>', views.prep_room, name='prep_room'),
     path('room/go/<str:room>', views.goto_room, name='goto_room'),
 
