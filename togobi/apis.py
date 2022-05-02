b739d83e6498d459a2de0e9330d8592ee1e47703
yowgerald <solongerald.personal.flexi@gmail.com>
2022-05-02 14:55:45 +0000

    separate the togobi apis

---
+++ togobi/apis.py
@@ -0,0 +1,178 @@
+from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
+from django.conf import settings
+from django.utils import timezone
+from django.db.models import Count, TextField
+from django.db.models.functions import Concat
+
+from togobi.models import Content, ContentFile, ContentJoin
+from togobi.serializers import ContentSerializer, ContentFileSerializer, ContentTopSerializer
+
+from google.auth.transport.requests import AuthorizedSession
+from google.resumable_media.requests import ResumableUpload
+from google.cloud import storage
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
+
+from pathlib import Path
+from pymediainfo import MediaInfo
+from datetime import timedelta, date
+import io
+import os
+
+time_threshold = timezone.now() + timedelta(hours=1)
+
+# TODO: return status codes of all api call
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def content_detail(request, id):
+    content = get_object_or_404(Content, id=id, user_id=request.user)
+    serializer = ContentSerializer(content)
+    return Response({
+        'result': serializer.data
+    })
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def content_collection(request):
+    # TODO: search also by location
+    query = request.GET.get('q')
+    if query:
+        lookups= (Q(title__icontains=query) | Q(description__icontains=query) | Q(tags__icontains=query)
+            | Q(user__username__icontains=query))
+        contents = Content.objects.filter(
+            lookups, target_date__gt=time_threshold).annotate(total_attendees=Count('contentjoin')).order_by('-target_date')
+    else:
+        contents = Content.objects.filter(
+            target_date__gt=time_threshold).annotate(total_attendees=Count('contentjoin')).order_by('-target_date')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(contents, 20)
+    contents = paginator.page(page)
+    serializer = ContentSerializer(contents, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def content_file_collection(request, id):
+    content_files = ContentFile.objects.filter(content=id, is_active=True)
+
+    file_type = request.GET.get('type')
+    if file_type:
+        content_files = content_files.filter(f_type=file_type)
+
+    content_files = content_files.order_by('id')
+
+    page = request.GET.get('page') or 1
+    paginator = Paginator(content_files, 10)
+    content_files = paginator.page(page)
+    next_page = prev_page = None
+    if content_files.has_other_pages():
+        if content_files.has_next():
+            next_page = content_files.next_page_number()
+        if content_files.has_previous():
+            prev_page = content_files.previous_page_number()
+    for cf in content_files:
+        cf.signed_url = __gen_signed_url(cf.source)
+    serializer = ContentFileSerializer(content_files, many=True)
+    return Response({
+            'next': next_page,
+            'previous': prev_page,
+            'result': serializer.data
+        })
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def content_file_delete(request, id):
+    if (id):
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(settings.GCP_BUCKET_NAME)
+
+        content_file = get_object_or_404(ContentFile, id = id)
+        blob = bucket.blob(content_file.source)
+        blob.delete()
+        content_file.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def contents_today(request):
+    cs_today = Content.objects.filter(
+            target_date__day=date.today().day).order_by('-target_date')
+    serializer = ContentSerializer(cs_today, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def contents_top(request):
+    # TODO: may need to return the number of attendees
+    t_contents = ContentJoin.objects.annotate(distinct_name=Concat(
+        'content_id', 'content_id', output_field=TextField())).order_by('distinct_name').distinct('distinct_name')
+    serializer = ContentTopSerializer(t_contents, many=True)
+    return Response(serializer.data)
+
+def __gen_signed_url(file):
+    url = None
+    if file is not None:
+        client = storage.Client()
+        bucket = client.get_bucket(settings.GCP_BUCKET_NAME)
+        blob = bucket.get_blob(file)
+        expiration = timezone.now() + timedelta(hours=1)
+        url = blob.generate_signed_url(expiration=expiration)
+    return url
+
+def get_filetype(file):
+    fileInfo = MediaInfo.parse(file)
+    _type = False
+    for track in fileInfo.tracks:
+        if track.track_type == "Image":
+            _type = track.track_type
+        elif track.track_type == "Video":
+            _type = track.track_type
+    return _type    
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def contentfile_upload(request, id):
+    # TODO: may need to be in try catch
+    storage_client = storage.Client()
+    transport = AuthorizedSession(credentials=storage_client._credentials)
+    bucket = settings.GCP_BUCKET_NAME
+
+    file = request.data.get('file', False)
+    if not file or not get_filetype(file):
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    f_type = get_filetype(file)
+    file.open()
+    data = file.read()
+    # file.seek(0, os.SEEK_END)
+    # fsize = file.tell()
+    # TODO: need to check the file size of the video, allowed size is 10mb of free users?
+    file.close()
+    url_template = (
+        u'https://www.googleapis.com/upload/storage/v1/b/{bucket}/o?uploadType=resumable')
+    upload_url = url_template.format(bucket=bucket)
+    chunk_size = 1024 * 1024  # 1MB or 256 x 1024 bytes (256 KB)
+    upload = ResumableUpload(upload_url, chunk_size)
+    stream = io.BytesIO(data)
+    ext = Path(file.name).suffix
+    file_source = settings.GCP_FOLDER_UPLOAD + "/" + "_".join(["file", timezone.now().strftime("%y%m%d_%H%M%S") + ext])
+    metadata = {u'name': file_source, }
+    content_type = u'image/png' # why static ?
+    response = upload.initiate(transport, stream, metadata, content_type)
+    content = get_object_or_404(Content, id=id)
+    while not upload.finished:
+        upload.transmit_next_chunk(transport)
+    content_file = ContentFile()
+    content_file.name = file.name
+    content_file.source = file_source
+    content_file.f_type = f_type
+    content_file.content = content
+    content_file.save()
+
+    serializer = ContentFileSerializer(content_file)
+    return Response({
+        'result': serializer.data
+    })
--- togobi/urls.py
+++ togobi/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from . import views
+from . import apis
 
 
 urlpatterns = [
     path('', views.content_list, name='home'),
     path('content/add', views.content_add, name='content_add'),
-    path('content/<int:id>', views.content_detail, name='content_detail'),
     path('content/<int:id>/join', views.content_join, name='content_join'),
     path('manage/contents', views.own_contents, name='own_contents'),
     path('manage/content/<int:id>/edit', views.own_content_edit, name='own_content_edit'),
@@ -14,10 +14,11 @@ urlpatterns = [
     path('manage/content/<int:id>/attendees', views.attendees_list, name='own_content_attendees'),
     path('manage/content/<int:id>/attendees/<int:attendee_id>/edit', views.attendee_edit, name='own_content_attendee_edit'),
     
-    path('contents', views.content_collection),
-    path('contents/today', views.contents_today),
-    path('contents/top', views.contents_top),
-    path('contents/<int:id>/content_files', views.content_file_collection),
-    path('contents/<int:id>/content_file/upload', views.contentfile_upload),
-    path('content_file/<int:id>/delete', views.content_file_delete),
+    path('content/<int:id>', apis.content_detail, name='content_detail'),
+    path('contents', apis.content_collection),
+    path('contents/today', apis.contents_today),
+    path('contents/top', apis.contents_top),
+    path('contents/<int:id>/content_files', apis.content_file_collection),
+    path('contents/<int:id>/content_file/upload', apis.contentfile_upload),
+    path('content_file/<int:id>/delete', apis.content_file_delete),
 ]
